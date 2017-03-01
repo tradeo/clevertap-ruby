d2efcdfbf9341988ab0941eb5c755ef0a85be57b
Kamen Kanev <kamen.e.kanev@gmail.com>
2017-03-01 16:44:05 +0200

    Add a option for a debug mode

--- README.md
+++ README.md
@@ -62,6 +62,14 @@ timestamp when the event is originally created.
 The value should respond to `.to_i` and return epoch time. It's passed with key *ts*.
 If it's missing the default is the current the time stamp.
 
+### Send debug requests
+Passing parameter `debug: true` to upload methods you can test the data submitted is without errors.
+The record won't be actually persisted.
+
+```ruby
+  CleverTap.upload_profile(profile, debug: true)
+```
+
 ### Handle the response
 
 The CleverTap response object the folloing interface:
--- lib/clever_tap/uploader.rb
+++ lib/clever_tap/uploader.rb
@@ -11,23 +11,28 @@ module CleverTap
       TYPE_PROFILE => 'profileData'
     }.freeze
 
-    attr_reader :records, :type, :identity_field, :date_field, :event_name
+    DEBUG_PARAMS = { dryRun: 1 }.freeze
+
+    attr_reader :records, :type, :identity_field, :date_field, :event_name, :debug
 
     # TODO: make defaults configurable
     # date_field should be a date object responding to `to_i` which
     # should returns epoch time
     # profile respond to .to_h
-    def initialize(records, identity_field: 'id', date_field: nil, event_name: nil)
+    def initialize(records, identity_field: 'id', date_field: nil, event_name: nil, debug: false)
       @type = event_name ? TYPE_EVENT : TYPE_PROFILE
       @records = records
 
       @identity_field = identity_field
       @date_field = date_field
       @event_name = event_name
+      @debug = debug
     end
 
     def call(client)
-      response = client.post(HTTP_PATH, build_request_body)
+      response = client.post(HTTP_PATH, build_request_body) do |request|
+        request.params.merge!(DEBUG_PARAMS) if debug
+      end
 
       parse_response(response)
     end
