d35c2af021b71c997b38345335adf10b578fb195
Svetoslav Blyahov <svetoslav@tradeo.com>
2018-03-21 18:23:53 +0200

    Update Faraday and fix failing tests/API changes

--- spec/vcr_config.rb
+++ spec/vcr_config.rb
@@ -2,7 +2,7 @@ VCR.configure do |config|
   config.cassette_library_dir = "#{File.dirname(__FILE__)}/vcr_cassettes"
   config.hook_into :faraday
   config.configure_rspec_metadata!
-  config.allow_http_connections_when_no_cassette = false
+  config.allow_http_connections_when_no_cassette = true
 
   config.before_record do |record|
     record.request.headers.merge!(
